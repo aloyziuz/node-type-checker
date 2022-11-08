a607281810ba842a13daa5a98f71ff5d7f7b39a7
Aloysius <aloyziuz@users.noreply.github.com>
2022-11-08 17:43:53 +0700

    updated the javascript file to es6 module

--- TypeChecker.js
+++ TypeChecker.js
@@ -1,58 +1,36 @@
-(function(){
-    var root = this;
-    var previous_typecheck = root.TypeCheck;
+/**
+ * will check whether a variable is null or undefined
+ * @param {any} obj 
+ * @returns {boolean}
+ */
+export function IsNotNullOrUndefined(obj){
+    return obj !== null && obj !== undefined;
+}
 
-    var TypeCheck = {
-        /**
-         * will check whether a variable is null or undefined
-         * @param {any} obj 
-         * @returns {boolean}
-         */
-        IsNotNullOrUndefined: function(obj){
-            return obj !== null && obj !== undefined;
-        },
+/**
+ * will check whether a variable is not empty and is a date instance and a valid date.
+ * @param {any} date 
+ * @returns {boolean}
+ */
+export function IsValidDate(date){
+    return IsNotNullOrUndefined(date) && 
+    Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
+}
 
-        /**
-         * will check whether a variable is not empty and is a date instance and a valid date.
-         * @param {any} date 
-         * @returns {boolean}
-         */
-        IsValidDate: function(date){
-            return module.exports.IsNotNullOrUndefined(date) && 
-            Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
-        },
+/**
+ * will check whether a variable is not empty and is a valid number
+ * @param {any} num 
+ * @returns {boolean}
+ */
+export function IsValidNumber(num){
+    return IsNotNullOrUndefined(num) && typeof(num) === "number" && !isNaN(num);
+}
 
-        /**
-         * will check whether a variable is not empty and is a valid number
-         * @param {any} num 
-         * @returns {boolean}
-         */
-        IsValidNumber: function(num){
-            return module.exports.IsNotNullOrUndefined(num) && typeof(num) === "number" && !isNaN(num);
-        },
-        
-        /**
-         * will check whether the string is empty or consist of white spaces.
-         * @param {string} str 
-         * @returns {boolean}
-         */
-        IsNonEmptyString: function(str){
-            return module.exports.IsNotNullOrUndefined(str) && str.toString().trim() !== "";
-        }
-    }
-
-    TypeCheck.noConflict = function(){
-        root.TypeCheck = previous_typecheck;
-        return TypeCheck;
-    }
-
-    if( typeof exports !== 'undefined' ) {
-        if( typeof module !== 'undefined' && module.exports ) {
-            exports = module.exports = TypeCheck;
-        }
-        exports.TypeCheck = TypeCheck;
-    } 
-    else {
-        root.TypeCheck = TypeCheck;
-    }
-}).call(this);
+/**
+ * will check whether the string is empty or consist of white spaces.
+ * @param {string} str 
+ * @returns {boolean}
+ */
+export function IsNonEmptyString(str){
+    return IsNotNullOrUndefined(str) && str.toString().trim() !== "";
+}
---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export function IsNotNullOrUndefined(obj: any): boolean;
+export function IsValidDate(date: any): boolean;
+export function IsValidNumber(num: any): boolean;
+export function IsNonEmptyString(str: string | null | undefined): boolean;
